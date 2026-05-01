2c660b444bd16d30b06ab34f5e5b288e9edbac25
zuleidedocarmo08-wq <zuleidedocarmo08@gmail.com>
2026-04-30 22:15:50 -0300

    Adiciona classe ItemPedido com cálculo de subtotal

---
+++ ItemPedido.java
@@ -0,0 +1,13 @@
+class ItemPedido {
+    private Produto produto;
+    private int quantidade;
+
+    public ItemPedido(Produto produto, int quantidade) {
+        this.produto = produto;
+        this.quantidade = quantidade;
+    }
+
+    public double calcularSubtotal() {
+        return produto.getPreco() * quantidade;
+    }
+}
