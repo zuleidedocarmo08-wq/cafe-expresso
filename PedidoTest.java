1962bdea967f5e377049e3033cf36587354b8cc7
zuleidedocarmo08-wq <zuleidedocarmo08@gmail.com>
2026-05-05 20:22:38 -0300

    Update PedidoTest.java

--- PedidoTest.java
+++ PedidoTest.java
@@ -7,7 +7,9 @@ public class PedidoTest {
             System.out.println("Teste pedido vazio OK");
         } else {
             System.out.println("Erro: pedido vazio deveria ser 0");
-            Pedido pedido2 = new Pedido();
+            
+        }
+Pedido pedido2 = new Pedido();
 
 pedido2.adicionarItem(new Produto("Café", 5.0));
 pedido2.adicionarItem(new Produto("Pão", 3.0));
@@ -20,7 +22,5 @@ if (total2 == 12.0) {
 } else {
     System.out.println("Erro no cálculo com múltiplos itens");
 }
-        }
-
     }
 }
