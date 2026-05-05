a53a311dc6fd05acb92c7c28be8787089bdc092d
zuleidedocarmo08-wq <zuleidedocarmo08@gmail.com>
2026-05-05 20:17:30 -0300

    Adiciona teste para pedido vazio

--- PedidoTest.java
+++ PedidoTest.java
@@ -1,20 +1,13 @@
 public class PedidoTest {
     public static void main(String[] args) {
 
-        Produto p1 = new Produto("Café", 5.0);
-        Produto p2 = new Produto("Pão", 3.0);
+        Pedido pedidoVazio = new Pedido();
 
-        Pedido pedido = new Pedido();
-
-        pedido.adicionarItem(p1);
-        pedido.adicionarItem(p2);
-
-        double total = pedido.calcularTotal();
-
-        if (total == 8.0) {
-            System.out.println("Total OK");
+        if (pedidoVazio.calcularTotal() == 0.0) {
+            System.out.println("Teste pedido vazio OK");
         } else {
-            System.out.println("Erro no total");
+            System.out.println("Erro: pedido vazio deveria ser 0");
         }
+
     }
 }
