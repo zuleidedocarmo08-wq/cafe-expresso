7fb7705b0220f43e901045c42da9f5368015ae43
zuleidedocarmo08-wq <zuleidedocarmo08@gmail.com>
2026-05-05 20:20:42 -0300

    Adiciona teste para múltiplos itens no pedido

--- PedidoTest.java
+++ PedidoTest.java
@@ -7,6 +7,19 @@ public class PedidoTest {
             System.out.println("Teste pedido vazio OK");
         } else {
             System.out.println("Erro: pedido vazio deveria ser 0");
+            Pedido pedido2 = new Pedido();
+
+pedido2.adicionarItem(new Produto("Café", 5.0));
+pedido2.adicionarItem(new Produto("Pão", 3.0));
+pedido2.adicionarItem(new Produto("Leite", 4.0));
+
+double total2 = pedido2.calcularTotal();
+
+if (total2 == 12.0) {
+    System.out.println("Teste múltiplos itens OK");
+} else {
+    System.out.println("Erro no cálculo com múltiplos itens");
+}
         }
 
     }
